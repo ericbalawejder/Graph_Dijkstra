f92b21635ab177a079742f017ed6e8b0c15b5f12
Eric Balawejder <eric.balawejder@gmail.com>
2016-12-11 13:59:00 -0500

    added pdf files

--- graph_shortest_path.cc
+++ graph_shortest_path.cc
@@ -3,7 +3,7 @@
 #include <limits.h>
 
 // Number of vertices in the graph
-#define Verticies 13
+#define Vertices 13
 
 // Function to find the vertex with minimum distance value from the set of 
 // vertices not yet included in the minimum spanning tree
@@ -13,7 +13,7 @@ int minimumDistance(int distance[], bool spanningSet[])
 	int minimum = INT_MAX; 
 	int minimum_index;
 
-	for (int vertex = 0; vertex < Verticies; vertex++)
+	for (int vertex = 0; vertex < Vertices; vertex++)
 	{ 
 		// if vertex is unvisited and distance < INT_MAX
 		if ((spanningSet[vertex] == false) && distance[vertex] <= minimum)
@@ -45,7 +45,7 @@ void print(int distance[], int n, int spanningTree[])
 {
 	int start = 0;
 	printf("Vertex\t   Distance\tPath");
-	for (int i = 1; i < Verticies; i++)
+	for (int i = 1; i < Vertices; i++)
 	{
 		printf("\n%d --> %d %8d %8d ", start, i, distance[i], start);
 		printSpanningTree(spanningTree, i);
@@ -55,19 +55,19 @@ void print(int distance[], int n, int spanningTree[])
 
 // Funtion that implements Dijkstra's algorithm for a graph represented 
 // using an adjacency matrix 
-void dijkstraAlgorithm(int graph[Verticies][Verticies], int start)
+void dijkstraAlgorithm(int graph[Vertices][Vertices], int start)
 {
 	// distance[] will hold the shortest distance[i] from start to i
-	int distance[Verticies]; 
+	int distance[Vertices]; 
 
 	// spanningSet[i] will be true if vertex i is included in the spanning tree
-	bool spanningSet[Verticies];
+	bool spanningSet[Vertices];
 
 	// spanningTree array to store shortest path tree
-	int spanningTree[Verticies];
+	int spanningTree[Vertices];
 
 	// Initialize all distances as infinite (INT_MAX) and spanningSet[] as false
-	for (int i = 0; i < Verticies; i++)
+	for (int i = 0; i < Vertices; i++)
 	{
 		// initialize to a vertex value not in the graph to start from
 		spanningTree[0] = -1;
@@ -79,7 +79,7 @@ void dijkstraAlgorithm(int graph[Verticies][Verticies], int start)
 	distance[start] = 0;
 
 	// Find shortest path for all vertices
-	for (int count = 0; count < Verticies - 1; count++)
+	for (int count = 0; count < Vertices - 1; count++)
 	{
 		// Pick the minimum distance vertex from the set of
 		// vertices not yet visted. u is always equal to start
@@ -90,7 +90,7 @@ void dijkstraAlgorithm(int graph[Verticies][Verticies], int start)
 		spanningSet[u] = true;
 
 		// Update distance value of the adjacent vertices of the picked vertex.
-		for (int vertex = 0; vertex < Verticies; vertex++)
+		for (int vertex = 0; vertex < Vertices; vertex++)
 		{
 			// Update distance[vertex] only if is not in spanningSet, there is
 			// an edge from u to vertex, and total weight of path from
@@ -105,13 +105,13 @@ void dijkstraAlgorithm(int graph[Verticies][Verticies], int start)
 	}
 
 	// print the distance values array
-	print(distance, Verticies, spanningTree);
+	print(distance, Vertices, spanningTree);
 }
 
 int main()
 {
-	
-	int graph[Verticies][Verticies] = 
+	// Define the graph
+	int graph[Vertices][Vertices] = 
 					{{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 					{1, 0, 15, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0},
 					{0, 15, 0, 0, 0, 0, 0, 4, 9, 0, 0, 0, 0},
